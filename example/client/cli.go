0b69b98ddebf119ba35af93672fecb9242246b49
exampleblob <127121150+exampleblob@users.noreply.github.com>
2020-02-17 22:51:27 +0000

    Rename post

--- example/client/cli.go
+++ example/client/cli.go
@@ -3,4 +3,14 @@ package client
 import (
 	"log"
 
-	"github
+	"github.com/jessevdk/go-flags"
+)
+
+func Run(args []string) {
+	options := &Options{}
+	_, err := flags.ParseArgs(options, args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = RunWithOptions(options); err !
