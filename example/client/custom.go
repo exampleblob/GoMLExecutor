30f3e04a987c46d71a06a74bf9dc2b6e91b40e61
exampleblob <127121150+exampleblob@users.noreply.github.com>
2020-03-03 20:23:30 +0000

    Update sports.m3u

--- example/client/custom.go
+++ example/client/custom.go
@@ -4,4 +4,12 @@ type customMakerRegistry struct {
 	registry map[string]func() interface{}
 }
 
-func (c *customMakerRegistry) Register(k string, gen func() interface{})
+func (c *customMakerRegistry) Register(k string, gen func() interface{}) (old func() interface{}, ok bool) {
+	if c.registry == nil {
+		c.registry = make(map[string]func() interface{})
+	}
+
+	old, ok = c.registry[k]
+	c.registry[k] = gen
+
+	return old, ok
