12113e65b31d305c19acf812c21fef67dc42e854
exampleblob <127121150+exampleblob@users.noreply.github.com>
2020-03-26 06:36:47 +0000

    Add Google Analytics

--- example/client/mlyc/mlyc.go
+++ example/client/mlyc/mlyc.go
@@ -6,4 +6,10 @@ import (
 	"github.com/viant/mly/example/client"
 	slfmodel "github.com/viant/mly/example/transformer/slf/model"
 	"github.com/viant/mly/shared/common"
-	"
+	"github.com/viant/mly/shared/common/storable"
+)
+
+func main() {
+	storableSrv := storable.Singleton()
+
+	// in actual client code, any type information should be available within the context of the caller, so 
