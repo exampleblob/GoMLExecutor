e9948b4866ff80a6d79e92e8955a522530bcbf73
exampleblob <127121150+exampleblob@users.noreply.github.com>
2020-03-28 21:41:57 +0000

    nightly autocommit

--- example/client/payload.go
+++ example/client/payload.go
@@ -32,3 +32,98 @@ func (c *CliPayload) Pair(key string, value interface{}) error {
 }
 
 func (c *CliPayload) SetBatch(msg *client.Message) {
+	if c.Batch > 0 {
+		msg.SetBatchSize(c.Batch)
+	}
+}
+
+// Bind delegates types to appropriate client.Message method.
+func (c *CliPayload) Bind(k string, value interface{}, msg *client.Message) error {
+	if c.Batch > 0 {
+		switch v := value.(type) {
+		case []int:
+			msg.IntsKey(k, v)
+		case []float32:
+			msg.FloatsKey(k, v)
+		case []string:
+			msg.StringsKey(k, v)
+		}
+	} else {
+		switch v := value.(type) {
+		case []int:
+			msg.IntKey(k, v[0])
+		case []float32:
+			msg.FloatKey(k, v[0])
+		case []string:
+			msg.StringKey(k, v[0])
+		}
+	}
+
+	return nil
+}
+
+// Parse expects the following format: "<input name>:[<type>|]<CSV values>[;<input name>:...]".
+// If type is not provided, then it will be string; otherwise, "int" and "float32" can be used.
+func Parse(p string, cp *CliPayload) error {
+	data := make(map[string]interface{})
+
+	chunks := strings.Split(p, ";")
+	for _, chunk := range chunks {
+		def := strings.Split(chunk, ":")
+		if len(def) != 2 {
+			return fmt.Errorf("chunk \"%s\" missing or has more than one \":\"", chunk)
+		}
+
+		field := def[0]
+		vals, err := csv.NewReader(strings.NewReader(def[1])).Read()
+		if err != nil {
+			return fmt.Errorf("csv error for field %s: %v", field, err)
+		}
+
+		valLen := len(vals)
+		if valLen > 1 {
+			if cp.Batch == 0 {
+				cp.Batch = valLen
+			} else if cp.Batch != valLen {
+				return fmt.Errorf("inconsistent batching at field %s", field)
+			}
+		}
+
+		typed := strings.Split(field, "|")
+		if len(typed) == 2 {
+			field = typed[0]
+			switch typed[1] {
+			case "int":
+				vs := make([]int, valLen)
+				for i, v := range vals {
+					cv, err := strconv.Atoi(v)
+					if err != nil {
+						return err
+					}
+
+					vs[i] = cv
+				}
+				data[field] = vs
+			case "float32":
+				vs := make([]float32, valLen)
+				for i, v := range vals {
+					cv, err := strconv.ParseFloat(v, 32)
+					if err != nil {
+						return err
+					}
+
+					vs[i] = float32(cv)
+				}
+				data[field] = vs
+			default:
+				return fmt.Errorf("unknown type:%s", typed[1])
+			}
+
+		} else {
+			data[field] = vals
+		}
+	}
+
+	cp.Data = data
+	return nil
+}
---
+++ example/client/runner.go
@@ -0,0 +1,123 @@
+
+package client
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/viant/gmetric"
+	"github.com/viant/mly/service/endpoint/checker"
+	"github.com/viant/mly/shared/client"
+	"github.com/viant/mly/shared/common/storable"
+	"github.com/viant/toolbox"
+)
+
+// Use CustomMakerRegistry with --maker to use a specific entity for Response.Data.
+var CustomMakerRegistry *customMakerRegistry = new(customMakerRegistry)
+
+func RunWithOptions(options *Options) error {
+	options.Init()
+	if err := options.Validate(); err != nil {
+		return err
+	}
+
+	if options.Model == "" {
+		return fmt.Errorf("could not determine model")
+	}
+
+	pls, err := options.Payloads()
+	if err != nil {
+		return err
+	}
+
+	if options.Debug {
+		fmt.Printf("payloads:%v\n", pls)
+	}
+
+	gm := gmetric.New()
+
+	opts := []client.Option{
+		client.WithDebug(options.Debug),
+		client.WithGmetrics(gm),
+	}
+
+	if options.CacheMB > 0 {
+		opts = append(opts, client.WithCacheSize(options.CacheMB))
+	}
+
+	if !options.NoHashCheck {
+		opts = append(opts, client.WithHashValidation(true))
+	}
+
+	cli, err := client.New(options.Model, options.Hosts(), opts...)
+	if err != nil {
+		return err
+	}
+
+	if options.PayloadDelay > 0 {
+		time.Sleep(time.Duration(options.PayloadDelay) * time.Second)
+	}
+
+	pPause := time.Duration(options.PayloadPause)
+	lp := len(pls)
+
+	concurrency := options.Concurrent
+
+	for i, upl := range pls {
+		pl := upl
+		payloadedRunner := func() error {
+			message := cli.NewMessage()
+			defer message.Release()
+
+			pl.SetBatch(message)
+			pl.Iterator(func(k string, value interface{}) error {
+				return pl.Bind(k, value, message)
+			})
+
+			response := &client.Response{}
+
+			var dataSetter func() interface{}
+
+			storableSrv := storable.Singleton()
+			maker, err := storableSrv.Lookup(options.Storable)
+			if err != nil {
+				if options.Debug {
+					fmt.Printf("could not find Storable:\"%s\", building dynamically\n", options.Storable)
+				}
+
+				maker = checker.Generated(cli.Config.Datastore.MetaInput.Outputs, pl.Batch, false)
+			}
+
+			dataSetter = func() interface{} {
+				return maker()
+			}
+
+			if options.CustomMaker != "" {
+				custom, ok := CustomMakerRegistry.registry[options.CustomMaker]
+				if !ok {
+					if options.Debug {
+						fmt.Printf("no such custom maker:\"%s\"\n", options.CustomMaker)
+					}
+				} else {
+					dataSetter = custom
+				}
+			}
+
+			response.Data = dataSetter()
+
+			ctx := context.Background()
+			cancel := func() {}
+			if options.TimeoutUs > 0 {
+				ctx, cancel = context.WithTimeout(ctx, time.Duration(options.TimeoutUs)*time.Microsecond)
+			}
+
+			err = cli.Run(ctx, message, response)
+
+			if err != nil && !options.SkipError {
+				cancel()
+				return err
+			}
+
+			cancel()
