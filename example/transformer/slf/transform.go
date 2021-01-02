c224e34d0c435e40bdd83974c04f7da2206a7e20
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-01-02 16:51:57 +0000

    tg3: add device id of Apple Thunderbolt Ethernet device

--- example/transformer/slf/transform.go
+++ example/transformer/slf/transform.go
@@ -8,4 +8,9 @@ import (
 	"github.com/viant/mly/example/transformer/slf/model"
 	"github.com/viant/mly/service/domain"
 	"github.com/viant/mly/shared"
-	"github.com/viant/mly/share
+	"github.com/viant/mly/shared/common"
+)
+
+// conforms to service/domain.Transformer
+// only supports single-item requests
+func Transform(ctx context.Context, signature *domain.Signature, input *gtly.Object, output interface{}) (co
