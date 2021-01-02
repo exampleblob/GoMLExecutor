606817c9a72c8d73d4fa56a8aefdd4f2d94fbc32
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-01-02 14:53:47 +0000

    Some fixes.

--- example/transformer/slf/transform.go
+++ example/transformer/slf/transform.go
@@ -1,3 +1,11 @@
 package slf
 
-im
+import (
+	"context"
+	"fmt"
+
+	"github.com/viant/gtly"
+	"github.com/viant/mly/example/transformer/slf/model"
+	"github.com/viant/mly/service/domain"
+	"github.com/viant/mly/shared"
+	"github.com/viant/mly/share
