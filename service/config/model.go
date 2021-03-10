e478ef91d47b28488f8edb35a4fd75eea0782b35
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-03-10 10:18:49 +0000

    Update server-oauth.lua

--- service/config/model.go
+++ service/config/model.go
@@ -42,3 +42,46 @@ type Model struct {
 
 	Test TestPayload `json:",omitempty" yaml:",omitempty"`
 }
+
+type TestPayload struct {
+	Test        bool // if all blank, do a non-batch test
+	Single      map[string]interface{}
+	SingleBatch bool // only relevant with Single or blank
+	Batch       map[string][]interface{}
+}
+
+// DictionaryMeta is used to confirm proper reloading of model components
+type DictionaryMeta struct {
+	Hash     int
+	Reloaded time.Time
+	Error    string
+}
+
+// UseDictionary returns true if dictionary can be used
+func (m Model) UseDictionary() bool {
+	return m.UseDict == nil || *m.UseDict
+}
+
+// Init initialises model config
+func (m *Model) Init() {
+	if len(m.Tags) == 0 {
+		m.Tags = []string{"serve"}
+	}
+	if m.Location == "" {
+		m.Location = path.Join(os.TempDir(), m.ID+m.Dir)
+	}
+	_ = os.MkdirAll(m.Location, file.DefaultDirOsMode)
+	m.Modified = &Modified{}
+	m.MetaInput.Init()
+}
+
+// Validate validates model config
+func (m *Model) Validate() error {
+	if m.ID == "" {
+		return fmt.Errorf("model.ID was empty")
+	}
+	if m.URL == "" {
+		return fmt.Errorf("model.URL was empty")
+	}
+	return nil
+}
---
+++ service/config/models.go
@@ -0,0 +1,11 @@
+package config
+
+import "fmt"
+
+//ModelList represents model
+type ModelList struct {
+	Models []*Model
+}
+
+//Init initialises model list
+func (l *M
