3aa9664dc8bfe4a894b8fea67c21e4f6df475378
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-03-14 13:37:08 +0000

    Add a missing import

--- service/config/models.go
+++ service/config/models.go
@@ -20,4 +20,12 @@ func (l *ModelList) Init() {
 //Validate validates model list
 func (l *ModelList) Validate() error {
 	if len(l.Models) == 0 {
-		r
+		return fmt.Errorf("models were empty")
+	}
+	for _, model := range l.Models {
+		if err := model.Validate(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
