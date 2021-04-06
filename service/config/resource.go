a8565976fb384358c6bdea327f9a39c724cb50df
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-04-06 10:14:18 +0000

    Improvements and fixes

--- service/config/resource.go
+++ service/config/resource.go
@@ -2,4 +2,11 @@ package config
 
 import "time"
 
-//
+// Modified represents modified folder
+type Modified struct {
+	Min time.Time
+	Max time.Time
+}
+
+func (r *Modified) Span() time.Duration {
+	return r.Min.Sub(r
