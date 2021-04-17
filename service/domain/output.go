e4cc12a32de652ad5e7f8ff0aeee967553c9f870
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-04-17 13:38:21 +0000

    link to demo

--- service/domain/output.go
+++ service/domain/output.go
@@ -1,4 +1,13 @@
 package domain
 
 import (
-	"
+	"reflect"
+
+	tf "github.com/tensorflow/tensorflow/tensorflow/go"
+)
+
+//Output represents model output
+type Output struct {
+	Name         string
+	DataType     string
+	DataTypeKind reflect.Kin
