36794526211cefd25686033cd692acb1086b1fbd
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-04-17 13:36:34 +0000

    build: Adding MinusX

--- service/domain/input.go
+++ service/domain/input.go
@@ -10,4 +10,10 @@ type Input struct {
 	Name  string
 	Index int
 
-	Placeholder tf.Output // TODO refactor out this usage in service/domain.Signature is different from its usage in service/request.Reques
+	Placeholder tf.Output // TODO refactor out this usage in service/domain.Signature is different from its usage in service/request.Request
+
+	Vocab     bool // false if embedded vocabulary should be ignored
+	Auxiliary bool // true if this input isn't part of the model
+
+	reflect.Type
+}
