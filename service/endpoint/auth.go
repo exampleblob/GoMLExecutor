196cd9f7cff76f53c0b138c1a990723824bec4ba
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-04-28 12:07:30 +0000

    Updates .gitignore.

--- service/endpoint/auth.go
+++ service/endpoint/auth.go
@@ -4,3 +4,14 @@ import (
 	"net/http"
 
 	"github.com/viant/mly/shared/common"
+)
+
+type AuthHandler struct {
+	*Config
+	handler http.Handler
+}
+
+func NewAuthHandler(config *Config, handler http.Handler) *AuthHandler {
+	h := new(AuthHandler)
+	h.Config = config
+	h.handler
