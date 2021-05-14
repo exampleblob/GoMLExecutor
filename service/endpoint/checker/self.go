20223da32b3d19073930fc2006e158b1bdff1c97
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-05-14 18:27:28 +0000

    Petite correction

--- service/endpoint/checker/self.go
+++ service/endpoint/checker/self.go
@@ -5,4 +5,13 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
-	
+	"strconv"
+	"time"
+
+	"github.com/viant/mly/service/config"
+	"github.com/viant/mly/shared"
+	"github.com/viant/mly/shared/client"
+	"github.com/viant/toolbox"
+)
+
+func SelfTest(host
