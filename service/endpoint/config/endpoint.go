1634d4129ba7e44201a2c2f0772632bc43d588a1
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-06-06 10:56:59 +0000

    Not necessary

--- service/endpoint/config/endpoint.go
+++ service/endpoint/config/endpoint.go
@@ -9,4 +9,7 @@ const defaultMaxEvaluatorWait = 50
 // Endpoint represents an endpoint
 type Endpoint struct {
 	Port           int
-	ReadTimeoutMs  int           `json:",omitempty" yam
+	ReadTimeoutMs  int           `json:",omitempty" yaml:",omitempty"`
+	WriteTimeoutMs int           `json:",omitempty" yaml:",omitempty"`
+	WriteTimeout   time.Duration `json:",omitempty" yaml:",omitempty"`
+	MaxHeaderBytes int           `json:",omitempty" yaml:",omi
