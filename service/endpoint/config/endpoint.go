268dff04e6287de9fd65a74d656d3ed8d3dbe71c
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-06-06 09:51:49 +0000

    Imported project

--- service/endpoint/config/endpoint.go
+++ service/endpoint/config/endpoint.go
@@ -1,3 +1,12 @@
 package config
 
-impor
+import (
+	"time"
+)
+
+const defaultMaxEvaluatorWait = 50
+
+// Endpoint represents an endpoint
+type Endpoint struct {
+	Port           int
+	ReadTimeoutMs  int           `json:",omitempty" yam
