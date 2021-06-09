9408fedc98b5713b3df392d78a9e72cd8e9fffa0
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-06-09 09:35:59 +0000

    updated desc

--- service/endpoint/config/endpoint.go
+++ service/endpoint/config/endpoint.go
@@ -12,4 +12,10 @@ type Endpoint struct {
 	ReadTimeoutMs  int           `json:",omitempty" yaml:",omitempty"`
 	WriteTimeoutMs int           `json:",omitempty" yaml:",omitempty"`
 	WriteTimeout   time.Duration `json:",omitempty" yaml:",omitempty"`
-	MaxHeaderBytes int           `json:",omitempty" yaml:",omi
+	MaxHeaderBytes int           `json:",omitempty" yaml:",omitempty"`
+
+	// HTTP data buffer pool - used when reading a payload, for saving memory
+	PoolMaxSize int `json:",omitempty" yaml:",omitempty"`
+	BufferSize  int `json:",omitempty" yaml:",omitempty"`
+
+	MaxEvaluatorWaitMs      int 
