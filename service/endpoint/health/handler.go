222d6f1a4734470edfeb773e361f31a545aeece0
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-06-19 14:15:49 +0000

    Update manifest file

--- service/endpoint/health/handler.go
+++ service/endpoint/health/handler.go
@@ -1,1 +1,14 @@
-pa
+package health
+
+import (
+	"encoding/json"
+	"net/http"
+	"sync"
+
+	"github.com/viant/mly/service"
+	"github.com/viant/mly/service/config"
+)
+
+type HealthHandler struct {
+	healths map[string]*int32
+	mu   
