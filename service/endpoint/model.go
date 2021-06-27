d0bcfbb04b7d4a0c105d3b2cf9c8dc2b2020e82c
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-06-27 10:14:08 +0000

    Update REPORT.md

--- service/endpoint/model.go
+++ service/endpoint/model.go
@@ -11,4 +11,8 @@ import (
 	"github.com/viant/afs"
 	"github.com/viant/gmetric"
 	"github.com/viant/mly/service"
-	"github.com/viant/mly/servi
+	"github.com/viant/mly/service/buffer"
+	"github.com/viant/mly/service/config"
+	serviceConfig "github.com/viant/mly/service/config"
+	"github.com/viant/mly/service/endpoint/meta"
+	"github.com/viant/mly/
