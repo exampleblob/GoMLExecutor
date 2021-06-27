93b12aa2b5934b339c2746d2d50cd2ab9945f09e
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-06-27 17:48:30 +0000

    adding tags

--- service/endpoint/model.go
+++ service/endpoint/model.go
@@ -15,4 +15,13 @@ import (
 	"github.com/viant/mly/service/config"
 	serviceConfig "github.com/viant/mly/service/config"
 	"github.com/viant/mly/service/endpoint/meta"
-	"github.com/viant/mly/
+	"github.com/viant/mly/shared/common"
+	"github.com/viant/mly/shared/datastore"
+	"golang.org/x/sync/semaphore"
+)
+
+type Hook interface {
+	Hook(*config.Model, *service.Service)
+}
+
+func Build(mux *http.ServeMux, config *Config, datasto
