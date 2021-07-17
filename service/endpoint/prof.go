c1d7d14b2273b633e6a7be951bceb4b96bdc3020
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-07-17 11:27:14 +0000

    Remove Debug

--- service/endpoint/prof.go
+++ service/endpoint/prof.go
@@ -12,4 +12,12 @@ const cpuProfIndexURI = "/v1/api/debug/pprof/"
 
 const cpuProfCmdlineURI = "/v1/api/debug/pprof/cmdline"
 const cpuProfProfileURI = "/v1/api/debug/pprof/profile"
-const cpuProfSymbolURI = "/v1/api/debu
+const cpuProfSymbolURI = "/v1/api/debug/pprof/symbol"
+const cpuProfTraceURI = "/v1/api/debug/pprof/trace"
+
+type memProfHandler struct {
+	l *sync.Mutex
+}
+
+func NewProfHandler() *memProfHandler {
+	return &memProfHandler{
