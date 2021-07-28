b05f85833e0a3cc3c69f6042e1f5ce94061e23e3
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-07-28 05:34:34 +0000

    Create LICENCE

--- service/files/service.go
+++ service/files/service.go
@@ -1,1 +1,12 @@
-package file
+package files
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/viant/afs"
+	"github.com/viant/afs/url"
+	"github.com/viant/mly/service/config"
+)
+
+// ModifiedSnapshot checks and updates modifie
