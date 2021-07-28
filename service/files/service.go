3d1a9d931402c49d02a6366ba2b884ea2d328286
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-07-28 15:41:45 +0000

    newspost

--- service/files/service.go
+++ service/files/service.go
@@ -11,4 +11,10 @@ import (
 
 // ModifiedSnapshot checks and updates modified times based on the object in URL
 func ModifiedSnapshot(ctx context.Context, fs afs.Service, URL string, resource *config.Modified) (*config.Modified, error) {
-	objects, err 
+	objects, err := fs.List(ctx, URL)
+	if err != nil {
+		return resource, fmt.Errorf("failed to list URL:%s; error:%w", URL, err)
+	}
+
+	if extURL := url.SchemeExtensionURL(URL); extURL != "" {
+		object, err := fs.Object(ctx, ex
