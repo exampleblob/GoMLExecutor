de299f3229f3a5cf07b32c376717691bcfa9a20a
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-08-02 07:19:02 +0000

    fixing javadoc warning

--- service/option.go
+++ service/option.go
@@ -3,4 +3,16 @@ package service
 import (
 	"time"
 
-	"github.com/vi
+	"github.com/viant/mly/shared/datastore"
+)
+
+type Option interface {
+	Apply(c *Service)
+}
+
+type storerOption struct {
+	storer datastore.Storer
+}
+
+func (o *storerOption) Apply(c *Service) {
+	c.datastore = o.
