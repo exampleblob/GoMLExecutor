0430a7350f042d2645ebff398494a0e7d0387319
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-08-03 15:54:49 +0000

    Improve indentation

--- service/request/request.go
+++ service/request/request.go
@@ -37,3 +37,198 @@ func NewRequest(keyLen int, inputs map[string]*domain.Input) *Request {
 // Put is used when constructing a request NOT using gojay.
 func (r *Request) Put(key string, value string) error {
 	if input, ok := r.inputs[key]; ok {
+		r.supplied[key] = exists
+
+		switch input.Type.Kind() {
+		case reflect.String:
+			r.Feeds[input.Index] = [][]string{{value}}
+		case reflect.Bool:
+			val, err := strconv.ParseBool(value)
+			if err != nil {
+				return fmt.Errorf("failed to parse bool: '%v' for %v, %w", val, key, err)
+			}
+			r.Feeds[input.Index] = [][]bool{{val}}
+		case reflect.Int:
+			val, err := strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				return fmt.Errorf("failed to parse int: '%v' for %v, %w", val, key, err)
+			}
+			r.Feeds[input.Index] = [][]int{{int(val)}}
+		case reflect.Int32:
+			val, err := strconv.ParseInt(value, 10, 32)
+			if err != nil {
+				return fmt.Errorf("failed to parse int32: '%v' for %v, %w", val, key, err)
+			}
+			r.Feeds[input.Index] = [][]int32{{int32(val)}}
+		case reflect.Int64:
+			val, err := strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				return fmt.Errorf("failed to parse int64: '%v' for %v, %w", val, key, err)
+			}
+			r.Feeds[input.Index] = [][]int64{{val}}
+		case reflect.Float64:
+			val, err := strconv.ParseFloat(value, 64)
+			if err != nil {
+				return fmt.Errorf("failed to parse float64: '%v' for %v, %w", val, key, err)
+			}
+			r.Feeds[input.Index] = [][]float64{{val}}
+		case reflect.Float32:
+			val, err := strconv.ParseFloat(value, 32)
+			if err != nil {
+				return fmt.Errorf("failed to parse float32: '%v' for %v, %w", val, key, err)
+			}
+			r.Feeds[input.Index] = [][]float32{{float32(val)}}
+		default:
+			// TODO add more type support
+			return fmt.Errorf("unsupported input type: %T", reflect.New(input.Type).Interface())
+		}
+	}
+
+	return nil
+}
+
+// implements gojay.UnmarshalJSONObject
+// see service.(*Handler).serveHTTP()
+// There is some polymorphism involved, as well as loose-typing.
+// Model inputs aren't strictly typed from the perspective of the request.
+// The primary polymorphism comes from the support for multiple rows worth of inputs.
+// There are 2 optional keys that aren't part of the model input: "batch_size" and "cache_key".
+// If the key "batch_size" is provided, then the keys' values should be an array of scalars; otherwise, the keys' values can be a single scalar.
+// The key "cache_key" is used for caching, and should be an array of strings if "batch_size" is provided.
+func (r *Request) UnmarshalJSONObject(dec *gojay.Decoder, key string) error {
+	if r.Input == nil {
+		r.Input = &transfer.Input{}
+	}
+
+	if r.supplied == nil {
+		r.supplied = make(map[string]struct{})
+	}
+
+	switch key {
+	case common.BatchSizeKey:
+		return dec.Int(&r.Input.BatchSize)
+	case common.CacheKey:
+		if r.Input.BatchMode() {
+			return dec.DecodeArray(&r.Input.Keys)
+		}
+		var k string
+		if err := dec.String(&k); err != nil {
+			return err
+		}
+		if err := r.Input.Keys.Set(k); err != nil {
+			return err
+		}
+	default:
+		input, ok := r.inputs[key]
+		if !ok {
+			return fmt.Errorf("unknown field:%v, available:%v", key, r.inputs)
+		}
+
+		r.supplied[key] = exists
+		inputValue, err := r.Input.SetAt(input.Index, input.Name, input.Type.Kind())
+		if err != nil {
+			return err
+		}
+
+		if r.Input.BatchMode() {
+			if err := dec.DecodeArray(inputValue); err != nil {
+				return err
+			}
+			if !input.Auxiliary {
+				r.Feeds[input.Index] = inputValue.Feed(r.Input.BatchSize)
+			}
+			return nil
+		}
+
+		outerr := func() error {
+			switch input.Type.Kind() {
+			case reflect.String:
+				var value string
+				if err := dec.String(&value); err != nil {
+					return err
+				}
+				_ = inputValue.Set(value)
+				if !input.Auxiliary {
+					r.Feeds[input.Index] = [][]string{{value}}
+				}
+			case reflect.Bool:
+				var value bool
+				if err := dec.Bool(&value); err != nil {
+					return err
+				}
+				if !input.Auxiliary {
+					r.Feeds[input.Index] = [][]bool{{value}}
+				}
+				_ = inputValue.Set(value)
+			case reflect.Int:
+				var value int
+				if err := dec.Int(&value); err != nil {
+					return err
+				}
+				if !input.Auxiliary {
+					r.Feeds[input.Index] = [][]int{{value}}
+				}
+				_ = inputValue.Set(value)
+			case reflect.Int32:
+				var value int32
+				if err := dec.Int32(&value); err != nil {
+					return err
+				}
+				if !input.Auxiliary {
+					r.Feeds[input.Index] = [][]int32{{value}}
+				}
+				_ = inputValue.Set(value)
+			case reflect.Int64:
+				var value int64
+				if err := dec.Int64(&value); err != nil {
+					return err
+				}
+				if !input.Auxiliary {
+					r.Feeds[input.Index] = [][]int64{{value}}
+				}
+				_ = inputValue.Set(value)
+			case reflect.Float64:
+				value := float64(0)
+				if err := dec.Float64(&value); err != nil {
+					return err
+				}
+				if !input.Auxiliary {
+					r.Feeds[input.Index] = [][]float64{{value}}
+				}
+				_ = inputValue.Set(value)
+			case reflect.Float32:
+				value := float64(0)
+				if err := dec.Float64(&value); err != nil {
+					return err
+				}
+				if !input.Auxiliary {
+					r.Feeds[input.Index] = [][]float32{{float32(value)}}
+				}
+				_ = inputValue.Set(value)
+			default:
+				return fmt.Errorf("unsupported request input type: %T", reflect.New(input.Type).Interface())
+			}
+
+			return nil
+		}()
+
+		if outerr != nil {
+			return fmt.Errorf("error decoding %s, %v", key, outerr)
+		}
+	}
+
+	return nil
+}
+
+// implements gojay.UnmarshalJSONObject
+func (r *Request) NKeys() int {
+	return 0
+}
+
+// Validate is only used server-side.
+// Extra fields are ignored.
+func (r *Request) Validate() error {
+	// TODO this isn't super accurate but it works as a quick check
+	if len(r.inputs) != len(r.supplied) {
+		missing := make([]string, 0)
+		for _, input := range r.inputs {
