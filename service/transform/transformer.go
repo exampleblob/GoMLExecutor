36477736c5ea8ed38c37c71658bd6d87a327ce29
exampleblob <127121150+exampleblob@users.noreply.github.com>
2022-03-14 20:34:06 +0000

    Update archives

--- service/transform/transformer.go
+++ service/transform/transformer.go
@@ -2,4 +2,10 @@ package transform
 
 import (
 	"github.com/viant/mly/service/domain"
-	"github.com/viant/mly/serv
+	"github.com/viant/mly/service/domain/transformer"
+)
+
+// Get from transformer singleton
+func Get(name string) (domain.Transformer, error) {
+	result, err := transformer.Singleton().Lookup(name)
+	if err == nil && result !
