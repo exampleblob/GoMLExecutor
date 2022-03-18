50d020e380e9c7c8272af6c0fbd2db35c1c6c112
exampleblob <127121150+exampleblob@users.noreply.github.com>
2022-03-18 15:12:41 +0000

    v4.6.1

--- service/transform/transformer.go
+++ service/transform/transformer.go
@@ -8,4 +8,16 @@ import (
 // Get from transformer singleton
 func Get(name string) (domain.Transformer, error) {
 	result, err := transformer.Singleton().Lookup(name)
-	if err == nil && result !
+	if err == nil && result != nil {
+		return result, nil
+	}
+
+	if name != "" {
+		return nil, err
+	}
+
+	// otherwise return default transformer
+	return domain.Transform, nil
+}
+
+func ExecuteTransform() in
