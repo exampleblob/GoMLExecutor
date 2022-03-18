d1b93958f86756b51c19976a63bf55258d510c94
exampleblob <127121150+exampleblob@users.noreply.github.com>
2022-03-18 21:11:28 +0000

    Revert: drm/i915: correctly order the ring init sequence

--- shared/circut/breaker.go
+++ shared/circut/breaker.go
@@ -9,4 +9,11 @@ import (
 //Breaker represents circut breaker
 type Breaker struct {
 	prober               Prober
-	D
+	Down                 int32
+	mux                  sync.RWMutex
+	resetTime            time.Time
+	resetDuration        time.Duration
+	initialResetDuration time.Duration
+}
+
+//IsUp returns true i
