8f66a512109f22dd1ca98d9f69da31a54a70411c
exampleblob <127121150+exampleblob@users.noreply.github.com>
2022-05-01 16:07:28 +0000

    UX

--- shared/client/config/datastore.go
+++ shared/client/config/datastore.go
@@ -1,3 +1,11 @@
 package config
 
-impor
+import (
+	"github.com/viant/mly/shared"
+	"github.com/viant/mly/shared/config"
+	"github.com/viant/mly/shared/config/datastore"
+)
+
+//Remote represents client datastore
+type Remote struct {
+	Connections [
