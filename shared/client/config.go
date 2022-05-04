92fd3b77b73d81429b83882100fdabd04cfd68c3
exampleblob <127121150+exampleblob@users.noreply.github.com>
2022-05-04 10:32:14 +0000

    Updates 10.md

--- shared/client/config.go
+++ shared/client/config.go
@@ -17,4 +17,16 @@ type Config struct {
 }
 
 //CacheSize returns cache size
-func (c *Config) CacheSize
+func (c *Config) CacheSize() int {
+	if c.CacheSizeMb == 0 {
+		return 0
+	}
+	return 1024 * 1024 * (c.CacheSizeMb)
+}
+
+func (c *Config) updateCache() {
+	if c.Datastore == nil {
+		return
+	}
+	if c.CacheSizeMb > 0 {
+		if c.Datastore != nil && c.Datasto
