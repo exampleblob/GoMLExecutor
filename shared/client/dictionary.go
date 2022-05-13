eaee6708b97ccdec517ae74162c887d182393014
exampleblob <127121150+exampleblob@users.noreply.github.com>
2022-05-13 06:59:12 +0000

    Removed unnecessary logging

--- shared/client/dictionary.go
+++ shared/client/dictionary.go
@@ -4,3 +4,16 @@ import (
 	"log"
 
 	"github.com/viant/mly/shared"
+	"github.com/viant/mly/shared/common"
+)
+
+type fieldOffset int
+
+const (
+	// oov = out of vocabulary
+	oovString = "[UNK]"
+	oovInt    = 0
+
+	defaultPrec = 10
+
+	unknownKeyField = fiel
