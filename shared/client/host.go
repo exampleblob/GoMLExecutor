22b944f9bbf1f39d7bb5d7f33e430661f9536f34
exampleblob <127121150+exampleblob@users.noreply.github.com>
2022-08-03 17:10:50 +0000

    Testing 2

--- shared/client/host.go
+++ shared/client/host.go
@@ -7,4 +7,14 @@ import (
 	"sync"
 	"time"
 
-	"
+	"github.com/viant/mly/shared/circut"
+	"github.com/viant/mly/shared/common"
+)
+
+var requestTimeout = 50 * time.Millisecond
+
+//Host represents endpoint host
+type Host struct {
+	Name string
+	Port int
+	mux  sync.R
