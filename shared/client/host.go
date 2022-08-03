d868ceeebf5e5b3a2b23bebf6d3d540d908d61a9
exampleblob <127121150+exampleblob@users.noreply.github.com>
2022-08-03 20:32:55 +0000

    Fixed broken test

--- shared/client/host.go
+++ shared/client/host.go
@@ -17,4 +17,14 @@ var requestTimeout = 50 * time.Millisecond
 type Host struct {
 	Name string
 	Port int
-	mux  sync.R
+	mux  sync.RWMutex
+	*circut.Breaker
+}
+
+//IsSecurePort() returns true if secure port
+func (h *Host) IsSecurePort() bool {
+	return h.Port%1000 == 443
+}
+
+//URL returns model eval URL
+func (h *Host) evalURL(model string) st
