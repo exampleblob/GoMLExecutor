b746e1e5d696d019eaf8efd728e234c9760b4342
exampleblob <127121150+exampleblob@users.noreply.github.com>
2022-12-14 14:15:12 +0000

    --warnings

--- shared/client/reconciler.go
+++ shared/client/reconciler.go
@@ -12,4 +12,9 @@ import (
 // ReconcileData reconciles target with cached and predicted data
 // target is either the pointer to the result or a pointer to a slice of results from
 // the prediction server
-func reconcileData(prefix string, target interface{}, cachable Cachable, ca
+func reconcileData(prefix string, target interface{}, cachable Cachable, cached []interface{}) error {
+	targetType := reflect.TypeOf(target).Elem()
+	targetPtr := xunsafe.AsPointer(target)
+
+	if prefix != "" {
+		log.Printf("%s reconciling: %T %+v", p
