eff49d50157e65a3189ca9777bced981764748d7
exampleblob <127121150+exampleblob@users.noreply.github.com>
2022-12-31 16:20:50 +0000

    Polish

--- shared/client/response.go
+++ shared/client/response.go
@@ -38,3 +38,74 @@ func (r *Response) UnmarshalJSONObject(dec *gojay.Decoder, key string) error {
 		serviceTime := 0
 		if err := dec.Int(&serviceTime); err != nil {
 			return err
+		}
+		r.ServiceTime = time.Duration(serviceTime) * time.Microsecond
+	case "data":
+		isEmpty := r.Data == nil
+
+		if !isEmpty {
+			if unmarshaler, ok := r.Data.(gojay.UnmarshalerJSONObject); ok {
+				return dec.Object(unmarshaler)
+			}
+
+			if unmarshaler, ok := r.Data.(gojay.UnmarshalerJSONArray); ok {
+				return dec.Array(unmarshaler)
+			}
+		}
+
+		var embedded = gojay.EmbeddedJSON{}
+		if err := dec.EmbeddedJSON(&embedded); err != nil {
+			return err
+		}
+
+		if isEmpty {
+			var aMap = make(map[string]interface{})
+			if err := json.Unmarshal(embedded, &aMap); err != nil {
+				return err
+			}
+			r.Data = aMap
+			return nil
+		} else if aMap, ok := r.Data.(map[string]interface{}); ok {
+			if err := json.Unmarshal(embedded, &aMap); err != nil {
+				return err
+			}
+			r.Data = aMap
+			return nil
+		}
+		if err := json.Unmarshal(embedded, r.Data); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *Response) DataItemType() (reflect.Type, error) {
+	if r.Data == nil {
+		return reflect.TypeOf(&struct{}{}), nil
+	}
+
+	dataType := reflect.TypeOf(r.Data)
+	// in go1.18 this was renamed to Pointer
+	if dataType.Kind() != reflect.Ptr && dataType.Kind() != reflect.Interface {
+		return nil, fmt.Errorf("expected Ptr or Interface, got %v", dataType)
+	}
+
+	elemType := dataType.Elem()
+	if elemType.Kind() == reflect.Slice {
+		return elemType.Elem(), nil
+	}
+
+	return elemType, nil
+}
+
+//NKeys returns object keys JSON (gojay API)
+func (r *Response) NKeys() int {
+	return 0
+}
+
+//NewResponse creates a new response
+func NewResponse(data interface{}) *Response {
+	return &Response{
+		Data: data,
+	}
+}
---
+++ shared/client/service.go
@@ -0,0 +1,11 @@
+package client
+
+import (
+	"bytes"
+	"context"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"
