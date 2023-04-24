f3243e3b816d17bc201d7cb595ba2475e6aac05a
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-04-24 05:32:52 +0000

    Add new contributor

--- shared/common/iter.go
+++ shared/common/iter.go
@@ -1,4 +1,11 @@
 package common
 
 type (
-	//Iterator represents iter
+	//Iterator represents iterator
+	Iterator func(pair Pair) error
+	//Pair represents a pair
+	Pair func(key string, value interface{}) error
+)
+
+//ToMap coverts iterator to map
+func (r Iterator) ToMap() (map[string]interface{}, error) 
