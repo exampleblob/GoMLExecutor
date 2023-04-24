40358a132e90d776c5faec5e1e699918b4979697
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-04-24 20:36:53 +0000

    Minor updates

--- shared/common/model.go
+++ shared/common/model.go
@@ -1,4 +1,9 @@
 package common
 
 const (
-	//ModelURI represents model eva
+	//ModelURI represents model eval URI template
+	ModelURI = "/v1/api/model/%s/eval"
+	//MetaURI represents model meta URI template
+	MetaURI = "/v1/api/model/%s/meta/"
+	//MetaConfigURI represents model meta config URI template
+	
