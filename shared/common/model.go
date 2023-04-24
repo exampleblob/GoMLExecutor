eddc9a93b8359bc27eeffc1a2578dbd753e096af
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-04-24 22:25:23 +0000

    Update import

--- shared/common/model.go
+++ shared/common/model.go
@@ -6,4 +6,7 @@ const (
 	//MetaURI represents model meta URI template
 	MetaURI = "/v1/api/model/%s/meta/"
 	//MetaConfigURI represents model meta config URI template
-	
+	MetaConfigURI = "/v1/api/model/%s/meta/config"
+	//MetaDictionaryURI represents model meta dictionary URI template
+	MetaDictionaryURI = "/v1/api/model/%s/meta/dictionary"
+)
---
+++ shared/common/status.go
@@ -0,0 +1,11 @@
+
+package common
+
+const (
+	//StatusOK response status ok
+	StatusOK = "ok"
+	//StatusError represents error status
+	StatusError = "error"
+	//StatusCached represents cache status
+	StatusCached = "cached"
+)
