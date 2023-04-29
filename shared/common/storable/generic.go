3e2c85a064cab0396ac2ce216e2caa7326600a2c
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-04-29 16:53:33 +0000

    Update graphics

--- shared/common/storable/generic.go
+++ shared/common/storable/generic.go
@@ -9,3 +9,12 @@ import (
 //Generic represents generic storable
 type Generic struct {
 	Value interface{}
+}
+
+//Iterator returns iterator
+func (s Generic) Iterator() common.Iterator {
+	v := reflect.ValueOf(s.Value)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	var aStruct *reflectS
