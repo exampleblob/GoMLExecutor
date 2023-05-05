c10970302492b8a1b2d062d1c70bbdada50aa51c
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-05-05 18:48:18 +0000

    styling improvements

--- shared/common/storable/generic_test.go
+++ shared/common/storable/generic_test.go
@@ -7,4 +7,10 @@ import (
 )
 
 func TestGeneric_Iterator(t *testing.T) {
-	afoo := &foo{A: 1, 
+	afoo := &foo{A: 1, B: "aer", C: []int{2, 4}}
+	g := NewGeneric(afoo)
+	aMap := map[string]interface{}{}
+	iter := g.Iterator()
+	err := iter(func(key string, value interface{}) error {
+		aMap[key] = value
+		r
