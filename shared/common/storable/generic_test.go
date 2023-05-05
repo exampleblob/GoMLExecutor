f22b69bac1f0986144e8bf828e8a6ae38fbe64fc
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-05-05 17:48:05 +0000

    [travis-ci] CS CHECK is up to 300% faster when xdebug is off

--- shared/common/storable/generic_test.go
+++ shared/common/storable/generic_test.go
@@ -1,1 +1,10 @@
-package storabl
+package storable
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/viant/mly/shared/common"
+	"testing"
+)
+
+func TestGeneric_Iterator(t *testing.T) {
+	afoo := &foo{A: 1, 
