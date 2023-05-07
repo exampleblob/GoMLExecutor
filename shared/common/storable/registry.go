8de6134223945c22bc90c7e13fd7bcb6196790af
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-05-07 09:34:27 +0000

    drm/i915: Fix DDC probe for passive adapters

--- shared/common/storable/registry.go
+++ shared/common/storable/registry.go
@@ -5,4 +5,11 @@ import (
 	"github.com/viant/mly/shared/common"
 )
 
-//Registry represents storable r
+//Registry represents storable registry
+type Registry struct {
+	registry map[string]func() common.Storable
+}
+
+//Register represents storable registry
+func (r *Registry) Register(key string, fn func() common.Storable) {
+	r.reg
