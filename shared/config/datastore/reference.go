f3799dcfe24c951d2bd01d226faafcb6f45bc0d4
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-05-12 14:59:34 +0000

    Bump version to 2.2.1

--- shared/config/datastore/reference.go
+++ shared/config/datastore/reference.go
@@ -3,4 +3,12 @@ package datastore
 import "time"
 
 const (
-	defaultTimeToLiveMs = 90000
+	defaultTimeToLiveMs = 900000 // 15 minutes
+	defaultRetryTimeMs  = 5000   // 5 seconds
+)
+
+//Reference datastore reference
+type Reference struct {
+	Connection   string
+	Namespace    string
+	Data
