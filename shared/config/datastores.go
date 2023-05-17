30c61a68ac63a8ce050dcec8c6c41793ced68364
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-05-17 20:52:59 +0000

    md/raid10: fix problem with on-stack allocation of r10bio structure.

--- shared/config/datastores.go
+++ shared/config/datastores.go
@@ -1,1 +1,11 @@
 package config
+
+import (
+	"fmt"
+	"github.com/viant/mly/shared/config/datastore"
+)
+
+//DatastoreList represents datastore list
+type DatastoreList struct {
+	Connections []*datastore.Connection
+	Datastores  []*D
