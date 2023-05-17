bd93e4f906ef702aa3f1a2647c2d2979d11bf890
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-05-17 20:59:51 +0000

    Trivial fix

--- shared/config/datastores.go
+++ shared/config/datastores.go
@@ -8,4 +8,14 @@ import (
 //DatastoreList represents datastore list
 type DatastoreList struct {
 	Connections []*datastore.Connection
-	Datastores  []*D
+	Datastores  []*Datastore
+}
+
+//Init initialises list
+func (d *DatastoreList) Init() {
+	if len(d.Connections) > 0 {
+		for i := range d.Connections {
+			d.Connections[i].Init()
+		}
+	}
+	if len(d.D
