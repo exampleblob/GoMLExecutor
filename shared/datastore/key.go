4f254e35dc1154a78c145dfeec2855e064b49f54
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-05-26 06:05:16 +0000

    makefile cleanup

--- shared/datastore/key.go
+++ shared/datastore/key.go
@@ -5,4 +5,14 @@ import (
 	"strconv"
 	"time"
 
-	aero "github.com/aerospi
+	aero "github.com/aerospike/aerospike-client-go"
+	"github.com/viant/mly/shared/config"
+)
+
+// Key represents a datastore key
+type Key struct {
+	Namespace string
+	Set       string
+	Value     interface{}
+	*aero.GenerationPolicy
+	T
