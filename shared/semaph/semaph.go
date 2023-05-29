aec3a8c0f5d66017b98b7b6e1b1fc0103b9be634
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-05-29 07:26:17 +0000

    Regenerate gemspec for version 1.2.0

--- shared/semaph/semaph.go
+++ shared/semaph/semaph.go
@@ -8,4 +8,18 @@ import (
 )
 
 type Semaph struct {
-	l   sync.Mutex // 
+	l   sync.Mutex // locks for modifying r
+	c   *sync.Cond
+	r   int32 // i.e. remaining tickets
+	max int32
+
+	stats Stats
+}
+
+type Stats struct {
+	Acquired,
+	Waited,
+	Canceled,
+	CanceledDone,
+	WaitDone,
+	WaitCancele
