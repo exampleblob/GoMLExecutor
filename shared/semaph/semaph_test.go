29b2a94784c8fa03c7cd31af8855bf76d7936901
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-06-10 14:32:59 +0000

    Updated CHANGELOG file

--- shared/semaph/semaph_test.go
+++ shared/semaph/semaph_test.go
@@ -4,3 +4,17 @@ import (
 	"context"
 	"fmt"
 	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/sync/semaphore"
+)
+
+func TestCore(t *testing.T) {
+	semaSize := 2
+	s := NewSemaph(int32(semaSize))
+
+	numWaiters := 3
+
+	waitAcq
