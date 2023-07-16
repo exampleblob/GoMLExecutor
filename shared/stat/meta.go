38c730057dbd4f94857cdf43985c71ee0f382231
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-07-16 07:00:33 +0000

    Added pod spec

--- shared/stat/meta.go
+++ shared/stat/meta.go
@@ -1,3 +1,14 @@
 package stat
 
-import "
+import "github.com/viant/gmetric/counter"
+
+// shared error-only multi operation tracker provider
+type errorOnly struct{}
+
+func (p errorOnly) Keys() []string {
+	return []string{
+		ErrorKey,
+	}
+}
+
+fun
