48971a556f070c2079f04dfe9bdd6f60bca509fd
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-07-21 14:46:44 +0000

    Bump maven-javadoc-plugin from 3.2.0 to 3.3.0

--- shared/stat/values.go
+++ shared/stat/values.go
@@ -2,4 +2,12 @@ package stat
 
 import (
 	"context"
-	"
+	"errors"
+
+	"github.com/viant/gmetric/stat"
+)
+
+// Values is a utility to pass multiple events to gmetric.
+type Values []interface{}
+
+func (v *Values) AppendError(err error) 
