47bbc7ad35d72c889d4dd7ca54145fa228e79b43
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-07-21 14:47:07 +0000

    Another update

--- shared/stat/values.go
+++ shared/stat/values.go
@@ -10,4 +10,14 @@ import (
 // Values is a utility to pass multiple events to gmetric.
 type Values []interface{}
 
-func (v *Values) AppendError(err error) 
+func (v *Values) AppendError(err error) {
+	if errors.Is(err, context.Canceled) {
+		v.Append(Canceled)
+	} else if errors.Is(err, context.DeadlineExceeded) {
+		v.Append(DeadlineExceeded)
+	} else {
+		v.Append(err)
+	}
+}
+
+func (v *Va
