1a63195b2711326ff9ecfe857a9a6d80e9c654cb
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-07-23 19:15:07 +0000

    Update xapi-scorm-profile.md

--- shared/stat/write.go
+++ shared/stat/write.go
@@ -1,1 +1,15 @@
-package 
+package stat
+
+import "github.com/viant/gmetric/counter"
+
+const (
+	L1Write = "L1Write"
+	L2Write = "L2Write"
+)
+
+type write struct{}
+
+func (p write) Keys() []string {
+	return []string{
+		ErrorKey,
+		
