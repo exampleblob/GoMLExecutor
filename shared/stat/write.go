baad81b2025794410b0f1d689b4d30d52a87cf3d
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-07-24 08:24:07 +0000

    added sleep

--- shared/stat/write.go
+++ shared/stat/write.go
@@ -12,4 +12,19 @@ type write struct{}
 func (p write) Keys() []string {
 	return []string{
 		ErrorKey,
-		
+		L1Write,
+		L2Write,
+	}
+}
+
+func (p write) Map(value interface{}) int {
+	if value == nil {
+		return -1
+	}
+
+	switch val := value.(type) {
+	case error:
+		return 0
+	case string:
+		switch val {
+		c
