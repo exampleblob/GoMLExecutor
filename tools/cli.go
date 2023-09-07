93e02175483e4ffbc3a5c9a56829a722f0948cc0
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-09-07 20:54:04 +0000

    MIPS: mm: Add compound tail page _mapcount when mapped

--- tools/cli.go
+++ tools/cli.go
@@ -62,3 +62,73 @@ func Discover(options *Options) error {
 	if err != nil {
 		return err
 	}
+
+	defer writer.Close()
+	if options.Operation == "dictHash" {
+		return FetchDictHash(writer, options.DestURL, fs)
+	}
+
+	model, err := LoadModel(context.Background(), options.SourceURL)
+	if err != nil {
+		return err
+	}
+
+	signature, err := tfmodel.Signature(model)
+	if err != nil {
+		return err
+	}
+
+	switch options.Operation {
+	case "signature":
+		return DiscoverSignature(writer, signature)
+	case "layers":
+		return discoverLayers(options, model, int64(0), fs)
+	case "config":
+		return DiscoverConfig(options.SourceURL, model, writer)
+	default:
+		return fmt.Errorf("unsupported option: '%v'", options.Operation)
+	}
+}
+
+// Deprecated: due to the changing of how hashing works, this is no longer supported or needed.
+// TODO: make a different utility for checking vocabulary extraction.
+func discoverLayers(options *Options, model *tf.SavedModel, fsh int64, fs afs.Service) error {
+	if ok, _ := fs.Exists(context.Background(), options.DestURL); ok {
+		_ = fs.Delete(context.Background(), options.DestURL)
+	}
+
+	var exportables []string
+	for _, candidate := range model.Graph.Operations() {
+		if strings.Contains(candidate.Name(), "LookupTableExportV2") {
+			exportables = append(exportables, candidate.Name())
+		}
+	}
+
+	dictionary, err := layers.DiscoverDictionary(model.Session, model.Graph, exportables)
+	if err != nil {
+		return err
+	}
+
+	dictionary.UpdateHash(fsh)
+
+	logger, err := log.New(&config.Stream{URL: options.DestURL}, "myID", afs.New())
+	if err != nil {
+		return err
+	}
+
+	provider := msg.NewProvider(100*1024*1024, 1, json.New)
+	aMessage := provider.NewMessage()
+	aMessage.PutInt("Hash", dictionary.Hash)
+	aMessage.PutObjects("Layers", toEncoders(dictionary.Layers))
+	logger.Log(aMessage)
+	aMessage.Free()
+	return logger.Close()
+}
+
+func toEncoders(l []common.Layer) []io.Encoder {
+	var layers = make([]io.Encoder, len(l))
+	for i := range l {
+		layers[i] = &l[i]
+	}
+	return layers
+}
---
+++ tools/lib.go
@@ -0,0 +1,12 @@
+package tools
+
+import (
+	"context"
+	"encoding/json"
+	sjson "encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path"
+	"refl
