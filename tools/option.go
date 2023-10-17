ba99b7c7bfa2e7dd903d8a700c67457615efc17e
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-10-17 13:21:18 +0000

    Update titles

--- tools/option.go
+++ tools/option.go
@@ -4,4 +4,5 @@ import "fmt"
 
 type Options struct {
 	Mode      string `short:"m" long:"mode" choice:"discover" choice:"run" description:"mode"`
-	
+	Operation string `short:"o" long:"opt" choice:"dictHash" choice:"signature" choice:"layers" choice:"config"`
+	SourceURL string `short:"s" long:"src" description:"source location, required by all opt where mode
