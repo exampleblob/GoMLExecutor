7fbf74782436f951db3a08d2a722f90f792b34bd
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-10-19 11:25:05 +0000

    Adding a license

--- tools/option.go
+++ tools/option.go
@@ -5,4 +5,9 @@ import "fmt"
 type Options struct {
 	Mode      string `short:"m" long:"mode" choice:"discover" choice:"run" description:"mode"`
 	Operation string `short:"o" long:"opt" choice:"dictHash" choice:"signature" choice:"layers" choice:"config"`
-	SourceURL string `short:"s" long:"src" description:"source location, required by all opt where mode
+	SourceURL string `short:"s" long:"src" description:"source location, required by all opt where mode=discover"`
+	DestURL   string `short:"d" long:"dest" description:"dest location, required if opt=layers"`
+	ConfigURL string `short:"c" long:"config" description:"required if mode=run"`
+}
+
+fun
