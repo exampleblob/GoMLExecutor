7806ec82fde698441b0606084e3dcb8c2cf2be6c
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-10-23 19:11:38 +0000

    rubocop upgrade and cleanup

--- tools/smasher/run.go
+++ tools/smasher/run.go
@@ -1,1 +1,20 @@
 package smasher
+
+import (
+	"context"
+	"log"
+	"runtime"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/viant/mly/shared/semaph"
+)
+
+type (
+	Server interface {
+		Stats() string
+	}
+
+	Client interface {
+		Do() 
