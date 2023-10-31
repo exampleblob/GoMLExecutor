4c16f3f4177f88e8bae2b1b608f56ee5b993cb4b
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-10-31 23:09:30 +0000

    Latest shared

--- tools/smasher/testcmd/main.go
+++ tools/smasher/testcmd/main.go
@@ -22,3 +22,40 @@ func (c *cl) Do() error {
 	atomic.AddUint64(&c.sent, 1)
 	return err
 }
+
+func (c *cl) Sent() uint64 {
+	return c.sent
+}
+
+func main() {
+	sleeper := new(smasher.Sleeper)
+	sleeper.D = 1 * time.Second
+
+	ls := new(smasher.Lambda)
+	ls.H = sleeper
+
+	port := 8889
+	srv := new(http.Server)
+	srv.Addr = fmt.Sprintf(":%d", port)
+	srv.Handler = ls
+
+	var serverErr error
+	go func() {
+		serverErr = srv.ListenAndServe()
+		if serverErr != nil && serverErr != http.ErrServerClosed {
+			log.Fatal(serverErr)
+		}
+	}()
+
+	smasher.Run(smasher.TestStruct{
+		Server: func() (smasher.Server, error) {
+			return ls, nil
+		},
+		Client: func() (smasher.Client, error) {
+			c := new(cl)
+			c.URL = fmt.Sprintf("http://localhost:%d", port)
+			c.cli = new(http.Client)
+			return c, nil
+		},
+	}, -1, 150000, 1*time.Second)
+}
